ce3056ac35346990ed96634f1ff7196b5721bfbb
Suaden.Rowland <suadenrowland@gmail.com>
2023-10-21 06:58:47 -0700

    initial commit

---
+++ __tests__/shuffle.test.js
@@ -0,0 +1,12 @@
+const shuffle = require("../src/shuffle");
+
+describe("shuffle should...", () => {
+  test('Checks that shuffle returns an array', () => {
+    let result = shuffle()
+    expect(result).toBe = Array
+  })
+  test('Checks to ensure everything has been shuffled', () => {
+    let result = shuffle()
+    expect(result).toBe = shuffle()
+  })
+});
---
+++ ec2.md
@@ -0,0 +1,5 @@
+## Paste your link below
+
+https://trello.com/b/RsRTeB1v/assessment-6
+
+
